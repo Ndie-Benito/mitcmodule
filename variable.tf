6870fc08dece7325f98ce15b21d451589bd82eaf
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-26 20:43:03 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,26 +1,21 @@
-variable "resource_group_name" {
-  description = "The name of the resource group in which to create the Virtual Network."
-  type        = string
-}
 
-variable "location" {
-  description = "The Azure region where the Virtual Network will be created."
-  type        = string
-}
 
-variable "vnet_name" {
-  description = "The name of the Virtual Network."
-  type        = string
-}
 
-variable "vnet_address_space" {
-  description = "The address space for the Virtual Network."
-  type        = list(string)
-}
 
-variable "subnets" {
-  description = "A map of subnets to create within the Virtual Network."
-  type = map(object({
-    address_prefixes = list(string)
-  }))
+
+
+
+
+
+variable "subscription_id"{
+  type=string
+}
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
 }
