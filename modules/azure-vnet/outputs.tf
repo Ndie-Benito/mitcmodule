75eaf04b2010d9d86d33b899af261e7af46ccc50
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-26 21:22:34 -0500

    Delete modules/azure-vnet directory

--- modules/azure-vnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_id" {
-  description = "The ID of the Virtual Network."
-  value       = azurerm_virtual_network.vnet.id
-}
-
-output "subnet_ids" {
-  description = "A map of subnet names to their IDs."
-  value       = { for k, v in azurerm_subnet.subnet : k => v.id }
-}
