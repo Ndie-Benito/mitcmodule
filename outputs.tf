b4be87eb8f1e14be4c02b54f298a75d29f1ce9ce
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-26 21:19:00 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vnet_id" {
-  description = "The ID of the Virtual Network."
-  value       = module.azure_vnet.vnet_id
+  description = "ID du réseau virtuel"
+  value       = module.network.vnet_id
 }
 
 output "subnet_ids" {
-  description = "A map of subnet names to their IDs."
-  value       = module.azure_vnet.subnet_ids
+  description = "IDs des sous-réseaux"
+  value       = module.network.subnet_ids
 }
